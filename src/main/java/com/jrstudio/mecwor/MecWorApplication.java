48badfae726fa1db4dbaaa8b03069b754634ebc2
JR Studio <jrstudio8087@gmail.com>
2023-10-10 11:57:52 +0200

    Iniciando proyecto

---
+++ src/main/java/com/jrstudio/mecwor/MecWorApplication.java
@@ -0,0 +1,13 @@
+package com.jrstudio.mecwor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MecWorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MecWorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jrstudio/mecwor/MecWorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrstudio.mecwor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MecWorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
